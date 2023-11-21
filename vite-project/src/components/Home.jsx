2de9599d298099f7a6b6402ced54288f9b53450c
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-11-22 01:13:26 +0530

    initial commit

---
+++ vite-project/src/components/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+const Home = () => {
+  return (
+    <div>
+      <Container>
+        <div>
+        <img src="/images/dice.png" alt="" />
+        </div>
+        
+        <div className='content'>
+            <h1>
+                DICE GAME
+            </h1>
+            <Button>Play</Button>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
+const Container = styled.div`
+max-width: 1182px;
+display: flex;
+margin: 0 auto;
+height: 100vh;
+align-items: center;
+
+.content{
+    h1{
+        font-size: 96px;
+        font-weight: 800;
+    
+    }
+}
+`
+
+
+const Button = styled.button`
+background-color: black;
+color: white;
+border: none;
+padding: 10px 20px;
+border-radius: 5px;
+cursor: pointer;
+font-weight: bold;
+margin-top: 20px;
+transition: 0.4s background ease-in;
+&:hover{
+    background-color: #f40612;
+    transition: 0.3s background ease-in;
+}
+`
