1a42f529104e2774cbbc01338dd8f9167b7e5aa2
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-11-23 02:22:25 +0530

    added the Number Selector Component

--- vite-project/src/components/Game.jsx
+++ vite-project/src/components/Game.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import Score from '../components/Score'
-
+import NumberSelector from './NumberSelector';
 const Game = ({ toggle }) => {
   return (
-    <main>
-    <Score />
-    </main>
+    <MainContainer>
+      <div className="Top-section">
+        <Score />
+        <NumberSelector />
+      </div>
+
+    </MainContainer>
   );
 }
 
 export default Game;
+
+const MainContainer = styled.main`
+.Top-section{
+  display: flex;
+  justify-content: space-around;
+  align-items: end;
+}
+
+`
